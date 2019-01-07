f6431ec4cd11cc4c2337a92b1691b6eac934889e
Mary Rys <rys.mary@gmail.com>
2019-01-06 18:41:43 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.scss';
 import apiKeys from '../db/apiKeys.json';
 import getAndPrintProjects from './components/projects-component';
 import createNavbar from './components/Navbar/navbar';
+import writeBio from './components/bio-component';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
   getAndPrintProjects();
+  writeBio();
 };
 
 initializeApp();
